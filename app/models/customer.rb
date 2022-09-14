541bba7b0734ebaf43d3d801b4ba352e96f24d8d
Eric Espindola <eric_e_2012@hotmail.com>
2022-09-14 06:03:49 -0700

    create customer, subscription, and tea models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :customer_subscriptions
   has_many :subscriptions, through: :customer_subscriptions
+
+  validates_presence_of :first_name, :last_name, :email, :address
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   has_many :customer_subscriptions
   has_many :customers, through: :customer_subscriptions
+
+  validates_presence_of :title, :price, :status, :frequency
+
+  enum status: { active: 0, cancelled: 1 }
 end
