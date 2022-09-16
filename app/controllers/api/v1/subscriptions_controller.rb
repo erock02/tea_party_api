598c25d5c613b8186a04c9947649c34d04e8c1b4
Eric Espindola <eric_e_2012@hotmail.com>
2022-09-16 06:24:26 -0700

    add sad path testing and update controller methods to raise exceptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,20 +1,38 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def index
-    @customer = Customer.find(params[:customer_id])
-    render json: @customer.to_json(include: :subscriptions)
+    begin
+      @customer = Customer.find(params[:customer_id])
+      render json: @customer.to_json(include: :subscriptions)
+    rescue ActiveRecord::RecordNotFound => e
+      render json: "Could not find customer with customer ID provided", status: 404
+    end
   end
 
   def create
-    @customer = Customer.find(params[:customer_id])
-    @customer.subscriptions.create(subscription_params)
-    render json: @customer.to_json(include: :subscriptions), status: :created
+    begin
+      @customer = Customer.find(params[:customer_id])
+      @sub = @customer.subscriptions.new(subscription_params)
+      if @sub.save
+        render json: @customer.to_json(include: :subscriptions), status: :created
+      else
+        render json: @sub.errors.full_messages, status: 400
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: "Could not find customer with customer ID provided", status: 404
+    end
   end
 
   def update
-    subscription = Subscription.find(params[:id])
-    subscription.update(subscription_params)
-    render json: subscription, status: :ok
+    begin
+      subscription = Subscription.find(params[:id])
+      subscription.update(subscription_params)
+      render json: subscription, status: :ok
+    rescue ArgumentError => e
+      render json: e
+    rescue ActiveRecord::RecordNotFound => e
+      render json: "Could not find subscription with subscription ID provided", status: 404
+    end
   end
 
   private
